5d158dbc02d59f1fcd4362a19b3e3bf4e6f96160
matthew <matthkang@gmail.com>
2023-07-04 16:03:57 -0700

    create array of potential characters for password

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,15 @@
 // Assignment code here
-let password = "";
+let constructedPassword = [];
 let passwordLength = 0;
 let lowerCaseType = false;
 let upperCaseType = false;
 let numericType = false;
 let specialType = false;
 
+let lowerCaseChars = "abcdefghijklmnopqrstuvwxyz".split("");
+let upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+let numericChars = "1234567890".split("");
+let specialChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split('');
 
 function setPasswordLength() {
   passwordLength = prompt("Choose a length between 8-128 for the length of the password");
@@ -24,10 +28,35 @@ function setCharacterType() {
   specialType = confirm("Would you like the password to contain special type characters?")
 }
 
+// create array of potential characters for password
+function constructPassword() {
+  if (lowerCaseType === true){
+    constructedPassword = constructedPassword.concat(lowerCaseChars);
+    console.log("lower case true");
+  }
+  if (upperCaseType === true){
+    constructedPassword = constructedPassword.concat(upperCaseChars);
+    console.log("upper case true");
+
+  }
+  if (numericChars === true){
+    constructedPassword = constructedPassword.concat(numericChars);
+    console.log("numeric case true");
+
+  }
+  if (specialChars === true){
+    constructedPassword = constructedPassword.concat(specialChars)
+    console.log("special case true");
+
+  }
+}
+
 // prompts to create password
 function generatePassword() {
   setPasswordLength();
   setCharacterType();
+
+  constructPassword();
 
   // not a single type selected
   if (lowerCaseType === false && upperCaseType === false && numericType === false && specialType === false){
